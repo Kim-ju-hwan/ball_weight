6d14cd91111adc9508068f7292fb3e6d86f0b515
rlawn0738@gmail.com <rlawn0738@gmail.com>
2021-11-16 00:39:21 +0900

    비교창 만들었습니다

--- ball_weight.cpp
+++ ball_weight.cpp
@@ -42,10 +42,12 @@ void set_game() {
 	int ballnum[9] = { 1,2,3,4,5,6,7,8,9 };
 	bool moved[6] = { false, false, false, false, false, false };
 	int choose =0;
-	int round = 1, can_try, pick_num = 0;
+	int round = 1, can_try=5, pick_num = 0;
 	int correct = 0, wrong = 0;
 	ball balls[6],pick[6];
 	ball moved_ball;
+	auto shown = Object::create("Images/비교창.png", scene, 320, 475);
+	shown->setScale(0.5f);
 	auto big= Object::create("Images/크다.png", scene, 455, 500);
 	auto small= Object::create("Images/작다.png", scene, 455, 500);
 	big->hide(); small->hide();
@@ -105,7 +107,7 @@ void set_game() {
 						obj[i]->locate(scene, x[i], 900);
 						moved[i] = false;
 					}
-					choose =choose-2;
+					choose =0;
 					big->hide();
 					small->hide();
 				}
@@ -116,6 +118,7 @@ void set_game() {
 						obj[i]->locate(scene, x[i], 900);
 						moved[i] = false;
 					}
+					shown->hide();
 					big->hide();
 					small->hide();
 					pick_num=0;
@@ -137,25 +140,16 @@ void set_game() {
 		balls[0].hide();
 		balls[5].hide();
 	}
-	else if (round < 7) {
-		can_try = 8;
-		balls[0].show();
-		balls[5].hide();
-	}
-	else {
-		can_try = 10;
-		balls[5].show();
-	}
-
+	
 	auto next_button= Object::create("Images/다음.png", scene, 750, 700);
 	next_button->setOnMouseCallback([&](ObjectPtr object, int x1, int y1, MouseAction action)->bool {
-		bool correct=true;
+		bool is_correct=true;
 		if (round < 4) {
 			for (int i = 0;i < 3;i++) {
 				if (pick[i].Is_big(pick[i + 1]))
 					continue;
 				else {
-					correct = false;
+					is_correct = false;
 					break;
 				}
 			}
@@ -165,7 +159,7 @@ void set_game() {
 				if (pick[i].Is_big(pick[i + 1]))
 					continue;
 				else {
-					correct = false;
+					is_correct = false;
 					break;
 				}
 			}
@@ -175,12 +169,12 @@ void set_game() {
 				if (pick[i].Is_big(pick[i + 1]))
 					continue;
 				else {
-					correct = false;
+					is_correct = false;
 					break;
 				}
 			}
 		}
-		if (correct)	correct++;
+		if (is_correct)	correct++;
 		else wrong++;
 		if (round < 3) {
 			can_try = 5;
@@ -197,12 +191,13 @@ void set_game() {
 			balls[5].show();
 		}
 		round++;
-		printf("%d %d %d\n", correct, wrong, round);
-		pick_num = 2,move_num=0;
-		for (int i = 0;i < 6;i++) moved[i] = false;
+		printf("%d %d %d\n", correct, wrong, round-1);
+		pick_num = 0,move_num=0;
+		for (int i = 0;i < 6;i++) moved[i] = false,pick[i]=balls[0];
 		choose = 0;
 		//무게와 공을 다시 랜덤 배정, 라운드에 따라  다음라운드 시도 횟수도 조정
 		if (round < 10) {
+			shown->show();
 			for (int i = 0; i < 9; i++) {
 				r = rand() % 9;
 				temp = ballnum[i];
@@ -220,7 +215,7 @@ void set_game() {
 				obj[i]->setImage(path);
 				balls[i].set(weight[i], obj[i]);
 				balls[i].locate(scene, x[i], 900);
-			}
+			}	
 		}
 		else {
 			sprintf(path, "정답 : %d  오답 : %d", correct,wrong);
@@ -236,4 +231,5 @@ int main()
 	setGameOption(GameOption::GAME_OPTION_INVENTORY_BUTTON, false);
 	setGameOption(GameOption::GAME_OPTION_MESSAGE_BOX_BUTTON, false);
 	set_game();
+	return 0;
 }
